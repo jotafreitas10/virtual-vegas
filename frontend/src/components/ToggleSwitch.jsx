da86a680990b8c9a49780d5955874329a169827e
DSCF15 <154894262+DSCF15@users.noreply.github.com>
2024-05-10 16:25:08 +0100

    ee

--- frontend/src/components/ToggleSwitch.jsx
+++ frontend/src/components/ToggleSwitch.jsx
@@ -4,24 +4,24 @@ const ToggleSwitch = () => {
     const [checked, setChecked] = useState(false);
   
     const handleChange = () => {
-      setChecked(!checked);
+        setChecked(!checked);
     };
   
     return (
-      <label className="flex items-center cursor-pointer">
-        <div className="relative">
-          <input
-            type="checkbox"
-            className="hidden"
-            checked={checked}
-            onChange={handleChange}
-          />
-          <div className={`toggle__line w-12 h-5 rounded-full shadow-inner ${checked ? 'bg-gray-400': 'bg-[#ccb681]'}`}></div>
-          <div
-            className={`toggle__dot absolute w-6 h-5 rounded-full shadow inset-y-0 bg-white ${checked ? 'left-0' : 'right-0'}`}
-          ></div>
-        </div>
-      </label>
+        <label className="flex items-center cursor-pointer">
+            <div className="relative border-2 border-[#ccb681] rounded-full p-1 transition-colors duration-300 ease-in-out hover:border-[#e7cc8d]">
+                <input
+                    type="checkbox"
+                    className="hidden"
+                    checked={checked}
+                    onChange={handleChange}
+                />
+                <div className={`toggle__line w-12 h-5 rounded-full shadow-inner ${checked ? 'bg-gray-400': 'bg-[#ccb681]'}`}></div>
+                <div
+                    className={`toggle__dot absolute w-6 h-6 rounded-full shadow inset-y-0 bg-white ${checked ? 'left-0' : 'right-0'}`}
+                ></div>
+            </div>
+        </label>
     );
 }
 
--- frontend/src/pages/Definicoes.jsx
+++ frontend/src/pages/Definicoes.jsx
@@ -6,15 +6,16 @@ import ToggleSwitch from '../components/ToggleSwitch';
 const   Definicoes = () => {
     return (
       <div>
-        <Header title="Definições" />
-        <div className='relative max-w-[1240px] mx-auto'>
-        <p className=' p-4 text-orange-300 text-xl md:text-2xl font-extrabold font-sarabun'>JOGAR </p>
-        <h2 className='font-bold text-3xl md:text-5xl'>Os nossos jogos</h2>
-        <ToggleSwitch />
-        </div>
-        
-        <Footer />
+      <Header title="Definições" />
+      <div className='max-w-[1240px] mx-auto'>
+          <h2 className='p-4 text-xl md:text-2xl font-extrabold font-sarabun'>Gestão de consentimento </h2>
+          <div className='font-normal p-4 text-lg md:text-xl flex items-center'>
+              <p>Pode alterar as suas preferências em qualquer notícia e promoções que vai receber da nossa parte, bem como os canais de comunicação.</p>
+              <p>Os nossos jogos</p><ToggleSwitch />
+          </div>
       </div>
+      <Footer />
+  </div>
     );
   };
   
