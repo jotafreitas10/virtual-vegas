de8da2d45ae2d086b621845a682d044aa1f5d4b0
DSCF15 <david15scf@gmail.com>
2024-05-10 22:40:31 +0100

    X

--- frontend/src/pages/Blackjack.jsx
+++ frontend/src/pages/Blackjack.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import GamesCard from '../components/GamesCard';
+
 
 const Blackjack = () => {
   return (
--- frontend/src/pages/Definicoes.jsx
+++ frontend/src/pages/Definicoes.jsx
@@ -8,17 +8,17 @@ const Definicoes = () => {
         <div>
             <Header title="Definições" />
             <div className='max-w-[1240px] mx-auto'>
-                <h2 className='p-4 text-3xl md:text-4xl font-extrabold font-sarabun'>Gestão de consentimento </h2>
+                <h2 className='p-4 text-2xl md:text-4xl font-extrabold font-sarabun'>Gestão de consentimento </h2>
                 <div className='font-bold p-4 text-lg md:text-2xl flex flex-col'>
                     <p>Pode alterar as suas preferências em qualquer notícia e promoções que vai receber da nossa parte, bem como os canais de comunicação.</p>
-                    <ul className='p-4 font-normal md:text-xl'>
+                    <ul className='p-4 font-normal text-base md:text-xl'>
                         <li className="flex justify-between items-center"> Quero receber informações sobre Bónus e Ofertas. Percebo que posso cancelar este serviço a qualquer momento.<ToggleSwitch /></li>
                         <li className="flex justify-between items-center"> Quero receber novidades e material promocional.<ToggleSwitch /></li>
                     </ul>
                 </div>
                 <div className='font-bold p-4 text-lg md:text-2xl flex flex-col'>
                     <p>Podes entrar em contacto comigo através dos seguintes canais:</p>
-                    <ul className='p-4 font-normal md:text-xl'>
+                    <ul className='p-4 font-normal text-base md:text-xl'>
                         <li className="flex justify-between items-center"> Email<ToggleSwitch /></li>
                         <li className="flex justify-between items-center"> Telefone<ToggleSwitch /></li>
                         <li className="flex justify-between items-center"> SMS<ToggleSwitch /></li>
