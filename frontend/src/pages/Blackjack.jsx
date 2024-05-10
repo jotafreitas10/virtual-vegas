85e339999dea2b42a81439445bac72bcae770921
DSCF15 <david15scf@gmail.com>
2024-05-10 22:57:20 +0100

    Update Blackjack.jsx

--- frontend/src/pages/Blackjack.jsx
+++ frontend/src/pages/Blackjack.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-
 const Blackjack = () => {
   return (
     <div>
       <Header title="Blackjack" />
-      <br></br>
-      <br></br>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
+        <iframe
+          src="https://free-slots.games/playtechslots/PremiumBlackJackSingleHand/index.html"
+          title="Premium BlackJack Single Hand - free slot"
+          width="600"
+          height="400"
+          style={{ border: 'none' }}
+        ></iframe>
+      </div>
       <Footer />
     </div>
   );
